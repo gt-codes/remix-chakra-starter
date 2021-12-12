73093fcfd266822056b84d85c27621fcedf93e01
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-12-11 18:11:14 -0600

    refactor: updated theme

--- theme.ts
+++ theme.ts
@@ -2,8 +2,16 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
 	colors: {
+		background: {
+			main: '#fafafa',
+			darker: '#f5f5f5',
+			darkest: '#e0e0e0',
+		},
+		primary: {
+			50: '#ede7f6',
+			600: '#5b37b3',
+		},
 		brand: {
-			bg: '#f5f5f5',
 			primary: '#0070f3',
 			accent: '#5E35B1',
 			text: '#222',
