573ce7308210a590cfc6c77eb999c6b7e8621dea
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-12-12 12:14:50 -0600

    refactor: added link to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,18 @@
 import { HStack, Text } from '@chakra-ui/react';
-import { NavLink } from 'remix';
+import { Link, NavLink } from 'remix';
 
 export default function Header() {
 	return (
 		<HStack as='nav' w='full' alignItems='center' justifyContent='space-between' p={4} shadow='sm'>
-			<Text bgGradient='linear(to-r, brand.primary, brand.accent)' bgClip='text' fontSize='xl' fontWeight='Bold'>
-				Awesome Notes
-			</Text>
+			<Link to='/'>
+				<Text
+					bgGradient='linear(to-r, brand.primary, brand.accent)'
+					bgClip='text'
+					fontSize='xl'
+					fontWeight='Bold'>
+					Awesome Notes
+				</Text>
+			</Link>
 			<HStack>
 				<NavLink to='/' end prefetch='intent'>
 					<Text py={1} px={3} rounded='md' _hover={{ bg: 'primary.50' }} color='primary.600'>
