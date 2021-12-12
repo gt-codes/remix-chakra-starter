f28c0e03dd21497b13374b8017c380303967a34a
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-12-12 11:50:30 -0600

    feat: added form validation and mock submission

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -12,13 +12,15 @@ import {
 	ModalFooter,
 	ModalHeader,
 	ModalOverlay,
+	Select,
 	Stack,
 	Text,
 	Textarea,
 	useDisclosure,
 } from '@chakra-ui/react';
-import { useRef } from 'react';
-import { ActionFunction, Form, redirect, useActionData } from 'remix';
+import { useEffect, useRef } from 'react';
+import { ActionFunction, Form, LoaderFunction, redirect, useActionData, useLoaderData, useTransition } from 'remix';
+import { createPost, getAllUsers } from '../../../lib';
 
 interface Errors {
 	title?: string;
@@ -27,45 +29,48 @@ interface Errors {
 
 export const action: ActionFunction = async ({ request }) => {
 	const body = await request.formData();
-	const title = body.get('title');
-	const content = body.get('content');
+	const userId = parseInt(body.get('userId') as string);
+	const title = body.get('title') as string;
+	const content = body.get('content') as string;
 
 	const errors: Errors = {};
 
 	if (!title) errors.title = 'Title is required';
 	if (!content) errors.content = 'Content is required';
 
-	console.log({ errors });
-
 	if (Object.keys(errors).length) return errors;
-
+	await createPost({ title, body: content, userId });
 	return redirect('/posts');
 };
 
-export default function Index() {
-	const errors = useActionData<Errors>();
+export const loader: LoaderFunction = async () => {
+	const users = await getAllUsers();
+	return users;
+};
 
+export default function Index() {
+	const users = useLoaderData();
+	const errors = useActionData<Errors>();
 	const { isOpen, onOpen, onClose } = useDisclosure();
-	const initialFocus = useRef<HTMLInputElement>(null);
+	const initialFocus = useRef<HTMLSelectElement>(null);
 	const finalFocus = useRef<HTMLButtonElement>(null);
 
+	const { state } = useTransition();
+	const loading = state === 'submitting';
+
+	useEffect(() => {
+		if (state === 'idle') isOpen && onClose();
+	}, [state]);
+
 	return (
-		<Center flexDir="column" w="full">
-			<Text textAlign="center" fontWeight="bold" fontSize={32}>
+		<Center flexDir='column' w='full'>
+			<Text textAlign='center' fontWeight='bold' fontSize={32}>
 				Hey there 👋
 			</Text>
-			<Text textAlign="center" fontSize={18} fontWeight="light">
+			<Text textAlign='center' fontSize={18} fontWeight='light'>
 				Select a post on the left to view it or create one below!
 			</Text>
-			<Button
-				mt={6}
-				px={24}
-				_hover={{}}
-				ref={finalFocus}
-				bg="primary.600"
-				color="white"
-				onClick={onOpen}
-			>
+			<Button mt={6} px={24} _hover={{}} ref={finalFocus} bg='primary.600' color='white' onClick={onOpen}>
 				Create a post
 			</Button>
 			<Modal
@@ -73,30 +78,35 @@ export default function Index() {
 				initialFocusRef={initialFocus}
 				finalFocusRef={finalFocus}
 				isCentered
-				onClose={onClose}
-			>
+				onClose={onClose}>
 				<ModalOverlay />
 				<ModalContent>
-					<Form action="." method="post">
+					<Form action='.' method='post'>
 						<ModalHeader>New Post</ModalHeader>
 						<ModalCloseButton />
 						<ModalBody>
 							<Stack spacing={4}>
-								<FormControl id="first-name">
+								<FormControl>
+									<FormLabel>User</FormLabel>
+									<Select ref={initialFocus} name='userId'>
+										{users?.map((user: any) => (
+											<option key={user.id} value={user.id}>
+												{user.name}
+											</option>
+										))}
+									</Select>
+								</FormControl>
+								<FormControl isInvalid={Boolean(errors?.title)}>
 									<FormLabel>Title</FormLabel>
-									<Input
-										name="title"
-										ref={initialFocus}
-										placeholder="Awesome note #1"
-									/>
+									<Input name='title' placeholder='Awesome note #1' />
 									<FormErrorMessage>{errors?.title}</FormErrorMessage>
 								</FormControl>
-								<FormControl id="first-name">
+								<FormControl isInvalid={Boolean(errors?.content)}>
 									<FormLabel>Content</FormLabel>
 									<Textarea
-										name="content"
-										resize="none"
-										placeholder="Blah blah awesome awesome blah blah"
+										name='content'
+										resize='none'
+										placeholder='Blah blah awesome awesome blah blah'
 									/>
 									<FormErrorMessage>{errors?.content}</FormErrorMessage>
 								</FormControl>
@@ -104,16 +114,10 @@ export default function Index() {
 						</ModalBody>
 
 						<ModalFooter>
-							<Button
-								type="button"
-								colorScheme="gray"
-								variant="ghost"
-								mr={3}
-								onClick={onClose}
-							>
+							<Button type='button' colorScheme='gray' variant='ghost' mr={3} onClick={onClose}>
 								Cancel
 							</Button>
-							<Button type="submit" colorScheme="blue">
+							<Button isLoading={loading} type='submit' colorScheme='blue'>
 								Create
 							</Button>
 						</ModalFooter>
