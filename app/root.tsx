3f3804905d8953006b1d628cfbd77c6865ddb63a
gt-codes <garrett1.tolbert@gmail.com>
2022-01-13 00:24:40 +0000

    refactor: consume context in root

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,18 @@
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useCatch } from 'remix';
-import { ChakraProvider } from '@chakra-ui/react';
+import { Center, Text, ChakraProvider } from '@chakra-ui/react';
 import { theme } from '~/theme';
 import Layout from '~/components/Layout';
+import ServerStyleContext from "~/context.server";
+import ClientStyleContext from "~/context.client";
+import { withEmotionCache } from "@emotion/react";
+import { ReactNode, useContext, useEffect } from 'react';
 
 export default function App() {
 	return (
 		<Document>
-			<ChakraProvider theme={theme}>
-				<Layout>
-					<Outlet />
-				</Layout>
-			</ChakraProvider>
+			<Layout>
+				<Outlet />
+			</Layout>
 		</Document>
 	);
 }
@@ -19,16 +21,14 @@ export function ErrorBoundary({ error }: { error: Error }) {
 	console.error(error);
 	return (
 		<Document title='Error!'>
-			<ChakraProvider theme={theme}>
-				<Layout>
-					<div>
-						<h1>There was an error</h1>
-						<p>{error.message}</p>
-						<hr />
-						<p>Hey, developer, you should replace this with what you want your users to see.</p>
-					</div>
-				</Layout>
-			</ChakraProvider>
+			<Layout>
+				<div>
+					<h1>There was an error</h1>
+					<p>{error.message}</p>
+					<hr />
+					<p>Hey, developer, you should replace this with what you want your users to see.</p>
+				</div>
+			</Layout>
 		</Document>
 	);
 }
@@ -36,49 +36,74 @@ export function ErrorBoundary({ error }: { error: Error }) {
 export function CatchBoundary() {
 	let caught = useCatch();
 
-	let message;
-	switch (caught.status) {
-		case 401:
-			message = <p>Oops! Looks like you tried to visit a page that you do not have access to.</p>;
-			break;
-		case 404:
-			message = <p>Oops! Looks like you tried to visit a page that does not exist.</p>;
-			break;
-
-		default:
-			throw new Error(caught.data || caught.statusText);
-	}
-
 	return (
 		<Document title={`${caught.status} ${caught.statusText}`}>
-			<ChakraProvider theme={theme}>
 				<Layout>
-					<h1>
-						{caught.status}: {caught.statusText}
-					</h1>
-					{message}
+					<Center h='full' w='full'>
+					<Text
+						bgGradient='linear(to-r, brand.primary, brand.accent)'
+						bgClip='text'
+						fontSize='6xl'
+						fontWeight='Bold'
+					>
+						Page Not Found
+					</Text>
+					</Center>
 				</Layout>
-			</ChakraProvider>
 		</Document>
 	);
 }
 
-function Document({ children, title }: { children: React.ReactNode; title?: string }) {
-	return (
-		<html lang='en'>
+interface DocumentProps {
+	children: ReactNode;
+	title?: string;
+}
+
+const Document = withEmotionCache(
+	({ children, title }: DocumentProps, emotionCache) => {
+		const serverSyleData = useContext(ServerStyleContext);
+	  	const clientStyleData = useContext(ClientStyleContext);
+  
+		// Only executed on client
+		useEffect(() => {
+			// re-link sheet container
+			emotionCache.sheet.container = document.head;
+			// re-inject tags
+			const tags = emotionCache.sheet.tags;
+			emotionCache.sheet.flush();
+			tags.forEach((tag) => {
+			(emotionCache.sheet as any)._insertTag(tag);
+			});
+			// reset cache to reapply global styles
+			clientStyleData.reset();
+		}, [])
+	
+		return (
+			<html lang="en">
 			<head>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='width=device-width,initial-scale=1' />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
 				{title ? <title>{title}</title> : null}
 				<Meta />
 				<Links />
+				{serverSyleData?.map(({ key, ids, css }) => (
+				<style
+					key={key}
+					data-emotion={`${key} ${ids.join(" ")}`}
+					// eslint-disable-next-line react/no-danger
+					dangerouslySetInnerHTML={{ __html: css }}
+				/>
+				))}
 			</head>
 			<body>
+				<ChakraProvider theme={theme}>
 				{children}
+				</ChakraProvider>
 				<ScrollRestoration />
 				<Scripts />
-				{process.env.NODE_ENV === 'development' && <LiveReload />}
+				{process.env.NODE_ENV === "development" && <LiveReload />}
 			</body>
-		</html>
-	);
-}
+			</html>
+		);
+	}
+)
