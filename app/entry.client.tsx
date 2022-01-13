f32486710206315f682c16f4176e455cd2e8f08f
gt-codes <garrett1.tolbert@gmail.com>
2022-01-13 00:23:56 +0000

    refactor: updated style context on client/server

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,32 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "remix";
 
-hydrate(<RemixBrowser />, document);
+import React from "react";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "~/createEmotionCache";
+import ClientStyleContext from "~/context.client";
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = React.useState(createEmotionCache());
+
+  function reset() {
+    setCache(createEmotionCache());
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  );
+}
+
+hydrate(
+  <ClientCacheProvider>
+    <RemixBrowser />
+  </ClientCacheProvider>,
+  document
+);
